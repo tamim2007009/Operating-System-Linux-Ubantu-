c646c310acfae72928e5ef3803b2ad882a0bf40f
tamim <tamim2007009@stud.kuet.ac.bd>
2024-11-18 12:56:29 +0600

    makefile example

---
+++ lab4/makefile(exp)/add.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"header.h"
+
+void add(int a, int b){
+  
+ // printf("\n PID of add function is %d \n",getpid());
+  printf("\n Sum of %d and %d is %d\n",a,b,a+b);
+}
---
+++ lab4/makefile(exp)/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"header.h"
+void hello(){
+  
+ // printf("\n PID of  hello function is %d \n",getpid());
+  printf("\n Hello, I am in hello function\n");
+}
